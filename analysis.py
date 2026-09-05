65733664b6a4908bb6bd50d9064b620d9dd0bc91
kashishhhhh-19 <kashishsingh422@gmail.com>
2026-09-05 21:24:04 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -5,7 +5,7 @@ import pandas as pd
 connection = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="MyNewPass@1234",
+    password="YOUR_MYSQL_PASSWORD",
     database="job_market"
 )
 
